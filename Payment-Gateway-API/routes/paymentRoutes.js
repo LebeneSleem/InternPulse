e4d7f25db28e1628a9265cc3b0eda8dc31bfa5a6
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2025-01-22 19:05:07 +0000

    Payment integration API

--- Payment-Gateway-API/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { initiatePayment, verifyPayment } = require("../controllers/paymentControllers");
-
-router.post("/payments", initiatePayment);
-router.get("/payments/:reference", verifyPayment);
-
-module.exports = router;
